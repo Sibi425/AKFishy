782d3a98bd52f0fba8128831ab77f09a68c38d44
DESKTOP-61LCS5U\schmi <sebastian.s3@gmx.de>
2018-08-11 01:22:20 +0200

    Added Fish class and some UI Elements

---
+++ AKFishy/Fish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKFishy
+{
+    class Fish
+    {
+        String name;
+        String location;
+        String Day;
+        String loot;
+
+
+    }
+}
